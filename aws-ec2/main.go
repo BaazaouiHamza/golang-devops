50b3ae1de42e9fca05d71e648defd0b12a63cee5
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2024-04-03 02:37:59 +0100

    launch an EC2 with GO

---
+++ aws-ec2/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
+func main() {
+	var (
+		instanceId string
+		err        error
+	)
+	ctx := context.Background()
+	if instanceId, err = createEC2(ctx, "us-east-1"); err != nil {
+		fmt.Printf("createEC2 error: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Instance id: %s\n", instanceId)
+}
+
+func createEC2(ctx context.Context, region string) (string, error) {
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		return "", fmt.Errorf("unable to load SDK config, %s", err)
+	}
+
+	ec2Client := ec2.NewFromConfig(cfg)
+
+	keyPairs, err := ec2Client.DescribeKeyPairs(ctx, &ec2.DescribeKeyPairsInput{
+		KeyNames: []string{"go-aws-demo"},
+	})
+	if err != nil && !strings.Contains(err.Error(), "InvalidKeyPair.NotFound") {
+		return "", fmt.Errorf("describeKeyPair error: %s", err)
+	}
+
+	if len(keyPairs.KeyPairs) == 0 {
+		keyPair, err := ec2Client.CreateKeyPair(ctx, &ec2.CreateKeyPairInput{
+			KeyName: aws.String("go-aws-demo"),
+		})
+		if err != nil {
+			return "", fmt.Errorf("createKeyPair error: %s", err)
+		}
+		err = os.WriteFile("go-aws-ec2.pem", []byte(*keyPair.KeyMaterial), 0600)
+		if err != nil {
+			return "", fmt.Errorf("writeFile error: %s", err)
+		}
+	}
+	imageOuput, err := ec2Client.DescribeImages(ctx, &ec2.DescribeImagesInput{
+		Filters: []types.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: []string{"ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"},
+			}, {
+				Name:   aws.String("virtualization-type"),
+				Values: []string{"hvm"},
+			},
+		},
+		Owners: []string{"099720109477"},
+	})
+	if err != nil {
+		return "", fmt.Errorf("DescribeImages error: %s", err)
+	}
+
+	if len(imageOuput.Images) == 0 {
+		return "", fmt.Errorf("imagesOuput.Images is of 0 length")
+	}
+
+	instance, err := ec2Client.RunInstances(ctx, &ec2.RunInstancesInput{
+		ImageId:      imageOuput.Images[0].ImageId,
+		KeyName:      aws.String("go-aws-demo"),
+		InstanceType: types.InstanceTypeT3Micro,
+		MinCount:     aws.Int32(1),
+		MaxCount:     aws.Int32(1),
+	})
+	if err != nil {
+		return "", fmt.Errorf("runInstances error :%s", err)
+	}
+
+	if len(instance.Instances) == 0 {
+		return "", fmt.Errorf("instances is of 0 length")
+	}
+
+	return *instance.Instances[0].InstanceId, nil
+}
